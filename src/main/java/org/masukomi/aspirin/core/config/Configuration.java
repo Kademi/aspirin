1f0b03b3fe96d5079df44a2794a449304c363e03
brad <brad@brad-new-bigpc>
2020-04-15 13:15:10 +1200

    https://github.com/Kademi/kademi-dev/issues/9334

--- src/main/java/org/masukomi/aspirin/core/config/Configuration.java
+++ src/main/java/org/masukomi/aspirin/core/config/Configuration.java
@@ -398,7 +398,7 @@ public class Configuration implements ConfigurationMBean {
         mailSessionProps.put(MAIL_SMTP_TIMEOUT, getDeliveryTimeout()); //Socket I/O timeout value in milliseconds. Default is infinite timeout.
 
         mailSessionProps.put("mail.smtp.starttls.enable", "true");
-        mailSessionProps.put("mail.smtp.starttls.required", "true");
+        mailSessionProps.put("mail.smtp.starttls.required", "false");
         mailSessionProps.put("mail.smtp.ssl.checkserveridentity", "false");
         mailSessionProps.put("mail.smtp.ssl.trust", "*");
         //mailSessionProps.put("mail.smtp.ssl.ciphersuites", "SSL_RSA_WITH_RC4_128_MD5 SSL_RSA_WITH_RC4_128_SHA TLS_RSA_WITH_AES_128_CBC_SHA TLS_DHE_RSA_WITH_AES_128_CBC_SHA TLS_DHE_DSS_WITH_AES_128_CBC_SHA SSL_RSA_WITH_3DES_EDE_CBC_SHA SSL_DHE_RSA_WITH_3DES_EDE_CBC_SHA SSL_DHE_DSS_WITH_3DES_EDE_CBC_SHA SSL_RSA_WITH_DES_CBC_SHA SSL_DHE_RSA_WITH_DES_CBC_SHA SSL_DHE_DSS_WITH_DES_CBC_SHA SSL_RSA_EXPORT_WITH_RC4_40_MD5 SSL_RSA_EXPORT_WITH_DES40_CBC_SHA SSL_DHE_RSA_EXPORT_WITH_DES40_CBC_SHA SSL_DHE_DSS_EXPORT_WITH_DES40_CBC_SHA TLS_EMPTY_RENEGOTIATION_INFO_SCSV");
